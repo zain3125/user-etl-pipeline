acc9545139fb7615e0ac3d3493125ab983161e27
zain3125 <zeine712342@gmail.com>
2025-12-30 23:52:23 +0200

    Make DAGs

--- dags/kafka_stream.py
+++ dags/kafka_stream.py
@@ -17,7 +17,7 @@ def get_data():
 def format_data(user_data):
     data = {}
     location = user_data['location']
-    data['id'] = uuid.uuid4()
+    data['id'] = str(uuid.uuid4()) 
     data['first_name'] = user_data['name']['first']
     data['last_name'] = user_data['name']['last']
     data['gender'] = user_data['gender']
@@ -30,24 +30,33 @@ def format_data(user_data):
     data['registered_date'] = user_data['registered']['date']
     data['phone'] = user_data['phone']
     data['picture'] = user_data['picture']['medium']
-
     return data
 
-# print(format_data(get_data()))
-
 def stream_data():
-    # Create kafka producer, connect to server, and max waiting 5s
-    producer = KafkaProducer(bootstrap_server=['broker:29092'], max_block_ms=5000)
-
-    # Send data for one minute
+    producer = KafkaProducer(bootstrap_servers=['broker:29092'], max_block_ms=5000)
     curr_time = time.time()
     while True:
         if time.time() > curr_time + 60:
             break
         try:
-            user_data = (format_data(get_data()))
-            
+            user_data = format_data(get_data())
+            # نستخدم التوبيك باسم 'user_created'
             producer.send('user_created', json.dumps(user_data).encode('utf-8'))
         except Exception as e:
             logging.error(f"error: {e}")
             continue
+
+default_args = {
+    'owner': 'zain',
+    'start_date': datetime(2023, 9, 3, 10, 00)
+}
+
+with DAG('user_automation',
+         default_args=default_args,
+         schedule='@daily',
+         catchup=False) as dag:
+
+    streaming_task = PythonOperator(
+        task_id='stream_data_from_api',
+        python_callable=stream_data
+    )
